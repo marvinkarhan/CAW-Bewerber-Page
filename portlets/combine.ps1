8d9fd81d8c199f873cdea96055eaf9e1e098c21e
Marvin Karhan <1821400@stud.hs-mannheim.de>
2021-04-21 10:41:59 +0200

    Add ps script to get combined css files

---
+++ portlets/combine.ps1
@@ -0,0 +1,10 @@
+Remove-Item -Path ./combined.css
+
+$allscripts = Get-ChildItem -Path  . -Filter "*.css" -Recurse
+
+New-Item -Path . -Name "combined.css" -ItemType "file" -Value ""
+foreach ($script in $allscripts) {
+  $content = Get-Content -Path $script.FullName
+  Add-Content -Path ./combined.css -Value "/* Styles from: $($script.Name) */"
+  Add-Content -Path ./combined.css -Value $content
+}
