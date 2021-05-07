0bdbd678437d01b639bf949d7e43d87d37ec79f6
Marvin Karhan <1821400@stud.hs-mannheim.de>
2021-05-07 12:42:50 +0200

    Revert ps1 changes

--- portlets/combine.ps1
+++ portlets/combine.ps1
@@ -1,18 +1,10 @@
 Remove-Item -Path ./combined.css
 
-$mobileScripts = Get-ChildItem -Path  . -Filter "mobile*.css" -Exclude "*master.css", "*outsourced.css" -Recurse
-$defaultScripts = Get-ChildItem -Path  . -Filter "*.css" -Exclude "mobile*.css", "master.css", "outsourced.css" -Recurse
+$allscripts = Get-ChildItem -Path  . -Filter "*.css" -Exclude "master.css", "outsourced.css" -Recurse
 
-$names = @("mobile_combined.css", "combined.css")
-$scripts = @($mobileScripts, $defaultScripts)
-
-Write-Output $name, $args[0], ($args[0] -eq "-m")
-
-for ( $i = 0; $i -lt 2; $i++ ) {
-  New-Item -Path . -Name $names[$i] -ItemType "file" -Value "" -Force
-  foreach ($script in $scripts[$i]) {
-    $content = Get-Content -Path $script.FullName
-    Add-Content -Path ./combined.css -Value "/* Styles from: $($script.Name) */"
-    Add-Content -Path ./combined.css -Value $content
-  }
+New-Item -Path . -Name "combined.css" -ItemType "file" -Value ""
+foreach ($script in $allscripts) {
+  $content = Get-Content -Path $script.FullName
+  Add-Content -Path ./combined.css -Value "/* Styles from: $($script.Name) */"
+  Add-Content -Path ./combined.css -Value $content
 }
